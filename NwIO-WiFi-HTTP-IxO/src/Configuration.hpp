341b62fd5f01971be0d532017b819194249fb605
Amael BERTEAU <amael.berteau@tech-easy.fr>
2022-05-18 22:52:36 +0200

    [NwIO-WiFi-HTTP-IxO] Copy code from NwIO-WiFi-HTTP

---
+++ NwIO-WiFi-HTTP-IxO/src/Configuration.hpp
@@ -0,0 +1,70 @@
+/**
+ * NwIO-WiFi-HTTP (c) by Amael BERTEAU
+ * 
+ * NwIO-WiFi-HTTP is licensed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * 
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+ */
+
+#include <Arduino.h>
+#include "Input.hpp"
+#include "Output.hpp"
+
+// WiFi
+const char* WIFI_SSID = "REPLACE_WITH_YOUR_SSID";
+const char* WIFI_PASSWORD = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Network settings
+IPAddress ip(192, 168, 0, 11);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+// HTTP Server
+IPAddress server(192, 168, 0, 39);
+int serverPort = 14999;
+
+// DeviceId
+String deviceId = "";
+
+// Inputs
+const unsigned long debounceDelay = 25;
+
+/*
+  Format : { <pin>, <invertLogic>, <debounceDelay> }
+    <pin> : GPIO Pin Number
+    <invertLogic> : Invert Logic
+    <debounceDelay> : Debounce delay
+*/
+InputConfig inputConfigs[] =
+{
+  { 32, false, debounceDelay },
+  { 33, false, debounceDelay },
+  { 25, false, debounceDelay },
+  { 26, false, debounceDelay },
+  { 27, false, debounceDelay },
+  { 14, false, debounceDelay },
+  { 12, false, debounceDelay },
+  { 13, false, debounceDelay }
+};
+
+// Outputs
+/*
+  Format : { <pin>, <invertLogic> }
+    <pin> : GPIO Pin Number
+    <invertLogic> : Invert Logic
+*/
+OutputConfig outputConfigs[] =
+{
+  { 23, false },
+  { 22, false },
+  { 21, false },
+  { 19, false },
+  { 18, false },
+  { 5, false },
+  { 17, false },
+  { 16, false },
+  { 4, false },
+  { 15, false }
+};
---
+++ NwIO-WiFi-HTTP-IxO/src/InputStateHttpSender.hpp
@@ -0,0 +1,30 @@
+/**
+ * NwIO-WiFi-HTTP (c) by Amael BERTEAU
+ * 
+ * NwIO-WiFi-HTTP is licensed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * 
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+ */
+
+#ifndef INPUT_STATE_HTTP_SENDER_HPP
+#define INPUT_STATE_HTTP_SENDER_HPP
+
+#include <Arduino.h>
+
+class InputStateHttpSender {
+public:
+  InputStateHttpSender(String hostname, int port, String deviceId);
+  void send(uint8_t id, bool state);
+
+private:
+  String _baseUrl;
+  String _deviceId;
+  String getAbsolutePath(String path);
+  String getUrl(String path);
+  String getPath(uint8_t id, bool state);
+  String getUrl(uint8_t id, bool state);
+};
+
+#endif

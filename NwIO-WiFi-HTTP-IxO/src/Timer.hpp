51cfce1f71c5d4b721af393ef3d6c82a00e23b65
Amael BERTEAU <amael.berteau@tech-easy.fr>
2022-05-22 00:09:08 +0200

    [NwIO-WiFi-HTTP-IxO] Adding class Timer

---
+++ NwIO-WiFi-HTTP-IxO/src/Timer.hpp
@@ -0,0 +1,28 @@
+/**
+ * NwIO-WiFi-HTTP-IxO (c) by Amael BERTEAU
+ * 
+ * NwIO-WiFi-HTTP-IxO is licensed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * 
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+ */
+
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <Arduino.h>
+
+class Timer {
+public:
+  void start(uint16_t duration);
+  bool isStarted();
+  bool hasElapsed();
+  void stop();
+
+private:
+  uint16_t _duration;
+  unsigned long _startMillis;
+};
+
+#endif
